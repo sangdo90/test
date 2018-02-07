54fd4855e86279e7a6b21764c68074d68e8f5685
pjk5401 <pjk5401@gmail.com>
2018-02-07 18:20:44 +0900

    Implement simple consensus

--- execute/consensus.go
+++ execute/consensus.go
@@ -1,7 +1,7 @@
 package execute
 
 import (
-	"fmt"
+	"strconv"
 
 	"github.com/smartm2m/blockchain/core"
 	"github.com/smartm2m/chainutil/console/command"
@@ -13,10 +13,27 @@ func ConsensusCommands() {
 	_ = command.AddCommand("blockchain", command.Command{
 		Name:        "copy",
 		ShortName:   "cp",
-		Description: "copy a new blockchain from existing blockchain",
+		Description: ": copy a new blockchain from existing blockchain",
 		Commands:    make([]command.Command, 0),
 		Flags:       nil,
 		Run:         CopyBlockchain,
+	})
+	_ = command.AddCommand("", command.Command{
+		Name:        "consensus",
+		ShortName:   "cs",
+		Description: ": consent then choose a blockchain from different blockchains",
+		Commands: []command.Command{
+			command.Command{
+				Name:        "execute",
+				ShortName:   "exec",
+				Description: ": execute consensus",
+				Commands:    make([]command.Command, 0),
+				Flags:       nil,
+				Run:         ExecuteConsensus,
+			},
+		},
+		Flags: nil,
+		Run:   nil,
 	})
 }
 
@@ -39,9 +56,51 @@ func CopyBlockchain(bcid uint64) error {
 	return nil
 }
 
-// Execution ...
-func Execution(p1 string, p2 int, p3 uint64, p4 int64, p5 []byte) error {
-	res := fmt.Sprintf("Execution(%s,%v,%v,%v,%v)", p1, p2, p3, p4, p5)
-	log.Debug(res)
+// execute consensus
+// ''ExecuteConsensus()''
+func ExecuteConsensus() error {
+	var maximum, eqaulnum, bcid uint64
+	maximum = 0
+	eqaulnum = 0
+	bcid = 0
+
+	for _, bc := range core.GlobalBlockchains {
+		if bc.BlockchainHeight > maximum {
+			maximum = bc.BlockchainHeight
+			bcid = bc.ID
+		}
+	}
+	for _, bc := range core.GlobalBlockchains {
+		if bc.BlockchainHeight == maximum {
+			eqaulnum = eqaulnum + 1
+		}
+	}
+
+	if eqaulnum > 1 {
+		log.Info("Chains are collide.")
+	} else {
+		log.Info("Choose blockchain ID" + strconv.FormatUint(bcid, 10))
+		ChooseBlockchain(bcid)
+	}
+
+	return nil
+}
+
+func ChooseBlockchain(bcid uint64) error {
+	bc, _ := core.SelectBlockchain(bcid)
+	//nbcid := uint64(len(core.GlobalBlockchains))
+	nbc := new(core.Blockchain)
+	nbc = &core.Blockchain{
+		ID:               0,
+		Blocks:           bc.Blocks,
+		BlockchainHeight: bc.BlockchainHeight,
+		GenesisBlock:     bc.GenesisBlock,
+		CandidateBlock:   bc.CandidateBlock,
+		TotalAmount:      bc.TotalAmount,
+	}
+	core.GlobalBlockchains = core.GlobalBlockchains[:0]
+	core.ChainsID = 0
+	core.GlobalBlockchains = append(core.GlobalBlockchains, nbc)
+
 	return nil
 }
